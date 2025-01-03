a9fadd31faf18f007ccf010963908730ab05e638
Nisha04Suthar <nishasuthar2002@gmail.com>
2025-01-03 13:23:26 +0530

    new icon added

--- src/common/Navbar.jsx
+++ src/common/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Navbar.css'
-import logo from '../images/caticon.png'
+import logo from '../images/black-cat.png'
 import { Link } from 'react-router-dom';
 
 function Navbar() {
